c1efe3c0ad358cb18f3c370f8f7d37017e1e4046
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-14 22:03:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size: 180%;
+background-size: 120%;
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
@@ -15,6 +15,13 @@ background-attachment: local;
 
 load_css()
 
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
 
 
 col1, col2, col3 = st.columns(3)
