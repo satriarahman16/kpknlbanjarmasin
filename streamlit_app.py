5cc2f645579a418588d25cd1526588698b638214
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-14 17:37:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ st.info('Kantor Pelayanan Kekayaan Negara dan Lelang Banjarmasin merupakan kanto
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
+st_button('doc', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
+st_button('doc', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
 st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
 st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
