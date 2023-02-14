b7117fdae9744ee166773a1407a76c42d280493c
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-14 17:41:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ st.info('Kantor Pelayanan Kekayaan Negara dan Lelang Banjarmasin merupakan kanto
 
 icon_size = 20
 
-st_button('doc', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('doc', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
+st_button('doc', 'https://linktr.ee/wadaimanis', 'Layanan Pengelolaan Kekayaan Negara', icon_size)
+st_button('doc', 'https://youtube.com/codingprofessor', 'Layanan Lelang', icon_size)
+st_button('medium', 'https://data-professor.medium.com/', 'Layanan Penilaian', icon_size)
+st_button('twitter', 'https://twitter.com/thedataprof/', 'Layanan Piutang Negara', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
