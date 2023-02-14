8af5ad1791185188cfd85d747cba608b7f4e227a
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-14 21:55:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ background-attachment: local;
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('djkn.png'))
