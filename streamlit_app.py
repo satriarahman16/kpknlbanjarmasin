f39663f300c281da44d50ba6b117b1f7cc016b93
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-15 08:05:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import base64
 import streamlit as st
 from st_functions import st_button, load_css
 from PIL import Image
-from streamlit_option_menu import option_menu
 
 @st.cache_data
 def get_img_as_base64(file):
