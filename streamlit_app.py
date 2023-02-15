244dd63dae13f6b416cc24afe5e0fbdc28c1daab
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-15 09:10:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,8 @@
-import base64 
+ 
 import streamlit as st
 from st_functions import st_button, load_css
 from PIL import Image
 
-@st.cache_data
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img = get_img_as_base64("image.jpg")
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size: 110%;
-background-position: top left;
-background-repeat no-repeat;
-background-attachment: local;
-}}
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
-</style>
-"""
 
         
 load_css()
@@ -50,7 +19,6 @@ st.info('Kantor Pelayanan Kekayaan Negara dan Lelang Banjarmasin merupakan kanto
 
 icon_size = 20
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
 st.sidebar.header("Configuration")
 st_button('doc', 'https://linktr.ee/wadaimanis', 'Layanan Pengelolaan Kekayaan Negara', icon_size)
 st_button('doc', 'https://youtube.com/codingprofessor', 'Layanan Lelang', icon_size)
