ed5211ab459e1f65436ea7e374d1814348b82e79
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-15 11:08:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,21 +21,3 @@ st_button('doc', 'https://linktr.ee/wadaimanis', 'Layanan Pengelolaan Kekayaan N
 st_button('doc', 'https://youtube.com/codingprofessor', 'Layanan Lelang', icon_size)
 st_button('medium', 'https://data-professor.medium.com/', 'Layanan Penilaian', icon_size)
 st_button('twitter', 'https://twitter.com/thedataprof/', 'Layanan Piutang Negara', icon_size)
-
-if __name__ == __main__:
-        main()
-
-
-st.session_state['answer'] = ''!
-
-st.write(st.session_state)
-
-realans = ['', 'abc', 'edf']
-
-if  st.session_state['answer'] in realans:
-    answerStat = "correct"
-elif st.session_state['answer'] not in realans:
-    answerStat = "incorrect"
-
-st.write(st.session_state)
-st.write(answerStat)
