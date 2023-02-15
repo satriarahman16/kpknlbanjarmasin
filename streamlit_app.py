c6ed981cbec0f5635a53ba941dbc6d9336253cf2
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-15 11:18:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from st_functions import st_button, load_css
 from PIL import Image
+import streamlit.components.v1 as components
         
 load_css()
 
@@ -23,3 +24,21 @@ st_button('medium', 'https://data-professor.medium.com/', 'Layanan Penilaian', i
 st_button('twitter', 'https://twitter.com/thedataprof/', 'Layanan Piutang Negara', icon_size)
 
 st.markdown("<h5 style='text-align: center; color: black;'>Ikuti Kami</h5>", unsafe_allow_html=True)
+
+
+
+
+
+components.html(
+    """
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" 
+        data-text="Check my cool Streamlit Web-App🎈" 
+        data-url="https://streamlit.io"
+        data-show-count="false">
+        data-size="Large" 
+        data-hashtags="streamlit,python"
+        Tweet
+        </a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    """
+)
