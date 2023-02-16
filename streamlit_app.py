3e8f21f1c9f06b1264b5164c29f46c0e9fc460f8
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-16 10:34:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_functions import st_button, load_css
+from st_functions import st_button, load_css, st_buttonmed
 from PIL import Image
 import streamlit.components.v1 as components
 
