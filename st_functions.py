22b7212cee8c9351d0f2b9f5d7c8627d1d08788b
satriarahman16 <96157360+satriarahman16@users.noreply.github.com>
2023-02-16 08:07:55 +0800

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -5,7 +5,7 @@ def load_css():
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-def st_button(icon, url, iconsize, label**):
+def st_button(icon, url, iconsize, label=" "):
     if icon == 'doc':
         button_code = f'''
         <p>
